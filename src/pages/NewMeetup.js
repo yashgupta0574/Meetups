206e2e9946c1026f4db2fa51bcd4d88deba652ed
Yash <yashdgupta2002@gmail.com>
2022-05-23 16:22:24 +0530

    added database

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch("https://meetups-2500e-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
